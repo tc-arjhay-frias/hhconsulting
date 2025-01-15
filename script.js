60ada45762382135acdc8b16c02cbbde0c65c2d8
Arjhay Frias <prehacks0942@gmail.com>
2025-01-16 05:03:40 +0800

    16-01-2025

--- script.js
+++ script.js
@@ -31,25 +31,24 @@
       fade: true,
     });
 
-    $('.links a[data-id="services"]').hover(() => {
-      $("#mega_nav > div").css("display", "none");
-      $("#mega_nav .nav_services").css("display", "flex");
-      $("#mega_nav").show();
-    });
-    $('.links a[data-id="projects"]').hover(() => {
-      $("#mega_nav > div").css("display", "none");
-      $("#mega_nav .nav_projects").css("display", "flex");
-      $("#mega_nav").show();
-    });
+    // $('.links a[data-id="services"]').hover(() => {
+    //   $("#mega_nav > div").css("display", "none");
+    //   $("#mega_nav .nav_services").css("display", "flex");
+    //   $("#mega_nav").show();
+    // });
+    // $('.links a[data-id="projects"]').hover(() => {
+    //   $("#mega_nav > div").css("display", "none");
+    //   $("#mega_nav .nav_projects").css("display", "flex");
+    //   $("#mega_nav").show();
+    // });
 
-    $("#mega_nav").mouseenter(() => {
-      $("#mega_nav").show();
-    })
+    // $("#mega_nav").mouseenter(() => {
+    //   $("#mega_nav").show();
+    // })
 
-    $("#mega_nav").mouseleave(() => {
-      $("#mega_nav").hide();
-
-    });
+    // $("#mega_nav").mouseleave(() => {
+    //   $("#mega_nav").hide();
+    // });
 
   });
 })();
